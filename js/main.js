023eb52f73faec452e8ec0bc90c0e6dda4a0ef09
Nick Fox-Gieg <nick@fox-gieg.com>
2013-04-18 14:41:49 -0400

    improved leap tracking

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 //~~~   1.  LEAP   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
 var fingers = {};
+var spheres = {};
 var pos = [ 0,0,0 ];
-//var pos = [ 0,0 ];
 
 Leap.loop(function(frame) {
   //try{  
     var fingerIds = {};
     var handIds = {};
+
+    //TRACK POINTABLES
+    /*
     for (var pointableId = 0, pointableCount = frame.pointables.length; pointableId != pointableCount; pointableId++) {
       var pointable = frame.pointables[pointableId];
        pos = [ pointable.tipPosition[0], pointable.tipPosition[1], pointable.tipPosition[2] ];
@@ -17,6 +20,28 @@ Leap.loop(function(frame) {
       //var finger = fingers[pointable.id]
       //var origin = new THREE.Vector3(pointable.tipPosition[0], pointable.tipPosition[1], -pointable.tipPosition[2])
       //var direction = new THREE.Vector3(pointable.direction[0], pointable.direction[1], -pointable.direction[2]);
+    }
+    */
+    //TRACK HANDS
+    if (frame.hands === undefined ) { 
+      var handsLength = 0 
+    } else {
+      var handsLength = frame.hands.length;
+    }
+
+    for (var handId = 0, handCount = handsLength; handId != handCount; handId++) {
+      var hand = frame.hands[handId];
+      var posX = (hand.palmPosition[0]);
+      var posY = (hand.palmPosition[1]);
+      var posZ = (hand.palmPosition[2]);
+      //var posX = (hand.palmPosition[0]*3);
+      //var posY = (hand.palmPosition[2]*3)-200;
+      //var posZ = (hand.palmPosition[1]*3)-400;
+      //var rotX = (hand.rotation[1][2]*90);
+      //var rotY = (hand.rotation[1][1]*90);
+      //var rotZ = (hand.rotation[1][0]*90);
+      pos = [ posX, posY, posZ ];
+      console.log(pos);
     }
   //}catch(e){}
 });
