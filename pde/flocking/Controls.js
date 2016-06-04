343d306daee8b1b94d72448e06da105e6caa12ca
Nick Fox-Gieg <nick@fox-gieg.com>
2016-06-04 18:35:41 -0400

    begin port to p5.js

---
+++ pde/flocking/Controls.js
@@ -0,0 +1,16 @@
+function keyPressed(){
+  if(key=='l' || key=='L') useLeap = !useLeap;
+
+  if(key==' '){
+  	if(!record & firstRun){
+  		obj1 = "";
+  		obj2 = "";
+  	}
+    record = !record;
+    firstRun=false;
+  }
+}
+
+function mousePressed() {
+  //
+}
